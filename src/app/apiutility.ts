55267a74fc1e2a91679d7ae6daa74fb92db2f364
Tarun Singh <tarun.singh1120@gmail.com>
2024-05-05 23:03:07 +0530

    apiurl

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,5 +1,2 @@
-// export const apiUrl = "http://143.198.235.166:8080";
-export const apiUrl = "http://localhost:8080";
-// export const apiUrl =
-//   "https://store.kindsmoke-53836ce6.eastus.azurecontainerapps.io/";
-// export const apiUrl = "http://172.178.84.112:8080";
+// export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'https://booking4.azurewebsites.net';
