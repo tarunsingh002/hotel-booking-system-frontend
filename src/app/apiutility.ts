8e2c8645869f2e6088469fc8d742bfb0860d03d8
Tarun Singh <tarun.singh1120@gmail.com>
2024-05-14 01:50:42 +0530

    api

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:8080';
-// export const apiUrl = 'https://booking4.azurewebsites.net';
+// export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'https://booking4.azurewebsites.net';
