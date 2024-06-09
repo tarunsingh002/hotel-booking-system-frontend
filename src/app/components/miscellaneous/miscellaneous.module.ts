6afc0c3d0de20f29dcfaeebfa2ac787b4cf09ca1
Tarun Singh <tarun.singh1120@gmail.com>
2024-06-09 23:35:47 +0530

    feedback

--- src/app/components/miscellaneous/miscellaneous.module.ts
+++ src/app/components/miscellaneous/miscellaneous.module.ts
@@ -1,30 +1,46 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule, Routes } from "@angular/router";
-import { AuthGuard } from "src/app/services/auth-services/auth.guard";
-import { CompleteComponent } from "./complete/complete.component";
-import { AboutusComponent } from "./aboutus/aboutus.component";
-import { ContactUsComponent } from "./contact-us/contact-us.component";
-import { FormsModule } from "@angular/forms";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from 'src/app/services/auth-services/auth.guard';
+import {CompleteComponent} from './complete/complete.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {FormsModule} from '@angular/forms';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {FeedbackRecordedComponent} from './feedback-recorded/feedback-recorded.component';
 
 const routes: Routes = [
   {
-    path: "complete",
+    path: 'complete',
     component: CompleteComponent,
     canActivate: [AuthGuard],
   },
   {
-    path: "contactus",
+    path: 'contactus',
     component: ContactUsComponent,
   },
   {
-    path: "aboutus",
+    path: 'aboutus',
     component: AboutusComponent,
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent,
+  },
+  {
+    path: 'recorded',
+    component: FeedbackRecordedComponent,
   },
 ];
 
 @NgModule({
-  declarations: [CompleteComponent, ContactUsComponent, AboutusComponent],
+  declarations: [
+    CompleteComponent,
+    ContactUsComponent,
+    AboutusComponent,
+    FeedbackComponent,
+    FeedbackRecordedComponent,
+  ],
   imports: [FormsModule, CommonModule, RouterModule.forChild(routes)],
 })
 export class MiscellaneousModule {}
