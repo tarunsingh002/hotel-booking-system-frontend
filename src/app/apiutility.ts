f11b8566923f307868e9c1f465903b2ed71577f5
Tarun Singh <tarun.singh1120@gmail.com>
2024-06-10 00:23:42 +0530

    api

--- src/app/apiutility.ts
+++ src/app/apiutility.ts
@@ -1,3 +1,3 @@
-export const apiUrl = 'http://localhost:8080';
+// export const apiUrl = 'http://localhost:8080';
 // export const apiUrl = 'https://booking4.azurewebsites.net';
-// export const apiUrl = 'https://booking.yellowrock-e27ddcf4.eastus.azurecontainerapps.io';
+export const apiUrl = 'https://booking.yellowrock-e27ddcf4.eastus.azurecontainerapps.io';
