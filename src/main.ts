e0dca0eba67a82fd17f6043efe913d20ca085101
Tarun Singh <tarun.singh1120@gmail.com>
2024-07-11 22:34:11 +0530

    dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {inject} from '@vercel/analytics';
 
 if (environment.production) {
   enableProdMode();
@@ -12,5 +11,3 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-inject();
